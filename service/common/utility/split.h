879679641fd6ca8736dd599a3bc4ea244181ce1a
Kirill Morozov (moki) <morozov.kirill.moki@gmail.com>
2021-01-29 20:31:06 +0300

    feat(common): debug and utilies functions
    debug:
            type_name - template for displaying cpp types.
    
    utility:
            split - split string into tokens by delimiter
    
            ltrim - trim whitespaces on the left
            rtrim - trim whitespaces on the right
            trim - trim whitespaces on both sides

---
+++ service/common/utility/split.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <sstream>
+#include <string>
+#include <vector>
+
+template <typename Out>
+void split(const std::string &s, char delim, Out result) {
+        std::istringstream iss(s);
+        std::string item;
+        while (std::getline(iss, item, delim)) {
+                if (!item.empty())
+                        *result++ = item;
+        }
+}
+
+std::vector<std::string> split(const std::string &s, char delim) {
+        std::vector<std::string> elems;
+        split(s, delim, std::back_inserter(elems));
+        return std::move(elems);
+}
